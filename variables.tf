d3ab5d9b33067e81092c25f7e3aaac084e52c2e5
Tania Kolesnik <kolesnik@gmail.com>
2026-05-16 11:54:56 +0100

    add

--- variables.tf
+++ variables.tf
@@ -2,13 +2,3 @@ variable "prefix" {
   type    = string
   default = "tfvmex"
 }
-
-variable "location" {
-  type    = string
-  default = "North Europe"
-}
-
-variable "vm_size" {
-  type    = string
-  default = "Standard_B2s_v2"
-}
